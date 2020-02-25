b8bd1f5d2f6bac17a0a7e2f9d66bec34f88767ae
kirsty <kirstypurkhardt@gmail.com>
2020-02-25 17:17:12 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="wrapper">
+
+        <h1 className="FAQ">Frequently Asked Questions</h1>
+
+        <div className="what"><p>What is Netflix?</p> 
+        <dropdownbutton className="drop" type="Dropdownbutton">+</dropdownbutton> 
+          </div>
+
+        <div className="how"><p>How much does Netflix cost?</p> 
+        <dropdownbutton className="down" type="Dropdownbutton">+</dropdownbutton>
+        </div>
+
+        <div className="where"><p>Where can I watch?</p> 
+        <dropdownbutton className="btn" type="Dropdownbutton">+</dropdownbutton> 
+        </div>
+
+        <div className="cancel"><p>How do I cancel?</p> 
+        <dropdownbutton className="new" type="Dropdownbutton">+</dropdownbutton> 
+        </div>
+
+        <div className="watch"><p>What can I watch on Netflix?</p> 
+        <dropdownbutton className="nw-btn" type="Dropdownbutton">+</dropdownbutton> 
+        </div>
+
+        <div className="trial"><p>How does the free trial work?</p> 
+        <dropdownbutton className="new-btn" type="Dropdownbutton">+</dropdownbutton> 
+        </div>
+
+      <div>
+      <button type="button">TRY 30 DAYS FREE > </button>
+      </div>
+
+      </div>
+    );
+  }
 }
 
 export default App;
